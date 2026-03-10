37b08931fbb5d3149da3613b5416e1aeba4b0ff5
variouslamp <sebsal2007@gmail.com>
2026-03-10 12:15:08 -0500

    feat(models): Add models of products and category

--- src/apps/product_app/models.py
+++ src/apps/product_app/models.py
@@ -1,3 +1,44 @@
+# This file is used to make the models fot the DB
+
 from django.db import models
 
-# Create your models here.
+# -----------------------------------------------------------------------------
+
+
+class Categoria(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    nombre = models.CharField(
+        max_length=100
+    )
+
+    def __str__(self):
+        return self.nombre
+
+
+# -----------------------------------------------------------------------------
+
+
+class Producto(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+    nombre = models.CharField(
+        max_length=150
+    )
+    costo = models.DecimalField(
+        max_digits=10, decimal_places=2
+    )
+    descripcion = models.TextField()
+    imagen = models.ImageField(
+        upload_to="productos/"
+    )
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE,
+        related_name="productos"
+    )
+
+    def __str__(self):
+        return self.nombre
